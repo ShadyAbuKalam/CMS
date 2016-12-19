dc54bea31e371b4337c477fa6c76082067a242d2
DoaaOsama <doaa_kandeel@hotmail.com>
2016-12-19 18:53:52 +0200

    CourseOfferings & Hours classes

---
+++ CMS/ICourseOffering.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace CMS
+{
+    interface ICourseOffering
+    {
+        string semster { get; set; }
+        int course_id { get; set; }
+
+        List<courseOffering> get_offerings();
+        void add_offerings(string sem, int id);
+
+        void update_offering(string sem, string osem, int oid, int id);
+
+        void delete_offering(string sem);
+
+
+    }
+}
