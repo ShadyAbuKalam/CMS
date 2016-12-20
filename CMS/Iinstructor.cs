c28e9e0f112f2fbbe70547ffa738142e766b2860
DoaaOsama <doaa_kandeel@hotmail.com>
2016-12-20 03:00:49 +0200

    Rasha

---
+++ CMS/Iinstructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS
+{
+    interface Iinstructor
+    {
+         string fullName { get; set; }
+         string email { get; set; }
+         string depName { get; set; }
+         string phone { get; set; }
+         int instructorId { get; set; }
+
+        List<Instructor> get_instructor();
+        void add_instructor(string fullName, int instructorId, string depName, string phone, string email);
+        void update_instructor(int instructorId, string newFullName, int newInstructorId, string newDepName, string newPhone, string newEmail);
+        void delete_instructor(int instructorId);
+    }
+}
---
+++ CMS/Instructor.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace CMS
+{
+    class Instructor: Iinstructor
+    {
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public string depName { get; set; }
+        public string phone { get; set; }
+        public int  instructorId { get; set; }
+
+
+            public List<Instructor> get_instructor()
+            {
+                List<Instructor> listInstructor = new List<Instructor>();
+                string connStr = " ";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                try
+                {
+                    conn.Open();
+                    // Perform database operations
+                    String DBreply = "SELECT * FROM Instructor";
+                    MySqlCommand cmd = new MySqlCommand(DBreply, conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                    Instructor instructor = new Instructor();
+                    instructor.instructorId = Convert.ToInt32(rdr["Instructor ID"]);
+                    instructor.fullName = rdr["full name"].ToString();
+                    instructor.email = rdr["Email"].ToString();
+                    instructor.phone = rdr["Phone"].ToString();
+                    instructor.depName = rdr["dep Name"].ToString();
+                    listInstructor.Add(instructor);
+                    }
+                    rdr.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                conn.Close();
+                return listInstructor;
+            }
+
+            public void add_instructor(string fullName, int instructorId, string depName, string phone, string email)
+        { 
+                string connStr = " ";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                try
+                {
+                    conn.Open();
+
+                    string sql = "INSERT INTO Instructor (FullName, ID , depName , Phone, Email ) VALUES ('" + fullName + "','" + instructorId + "','" + depName + "','" + phone + "','" + email + "');";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                conn.Close();
+
+            }
+            public void update_instructor(int instructorId, string newFullName, int newInstructorId, string newDepName, string newPhone, string newEmail)
+            {
+                string connStr = " ";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                try
+                {
+                    conn.Open();
+
+                    string sql = "UPDATE Instructor SET InstructorId ='" + instructorId + "', depName ='" + newDepName + "', Phone ='" + newPhone + "', Email ='" + newEmail + "', fullName ='" + newFullName + "' WHERE InstructorId='" + instructorId + "' ;";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                conn.Close();
+            }
+
+            public void delete_instructor(int instructorId)
+            {
+                string connStr = " ";
+                MySqlConnection conn = new MySqlConnection(connStr);
+                try
+                {
+                    conn.Open();
+
+                    string sql = "DELETE FROM Instructor WHERE InstructorId = '" + instructorId + "';";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                conn.Close();
+            }
+
+        }
+    }
+
