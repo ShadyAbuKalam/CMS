69987df78dc1423687f45f0aa5b649af78851092
Shady Atef <shady.atef@live.com>
2016-12-22 19:06:14 +0200

    Added DB helpers for Teaches relation

---
+++ CMS/Database/ITeaches.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMS.Models;
+
+namespace CMS.Database
+{
+    interface ITeaches
+    {
+        List<CourseOffering> GetCourseOfferingsByInstructor(Instructor instructor);
+        List<CourseOffering> AddCourseOfferingToInstructor(Instructor instructor,CourseOffering offering);
+    }
+}
